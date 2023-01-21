e33a06dcc6294c230a0fd3a247b9a20415116def
Muhammed Berk DİNCER <m.berkdincer@hotmail.com>
2023-01-21 15:45:37 +0300

    TaskManager_API update

---
+++ Section6_TaskManagerAPI/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 5000;
+const tasks = require('./routes/tasks')
+
+//middleware
+app.use(express.json())
+
+//routes
+app.use('/api/v1/tasks', tasks)
+
+
+app.get('/hello', (req,res)=>{
+    res.send('Hello friendly neighborhood')
+})
+
+
+
+app.listen(port)
---
+++ Section6_TaskManagerAPI/controllers/tasks.js
@@ -0,0 +1,27 @@
+const getAllTasks = (req,res)=>{
+    res.send('all items from the file')
+}
+
+const createTask = (req,res)=>{
+    res.send('al items frm the file')
+}
+
+const getSingleTask = (req,res)=>{
+    res.send('al items from the file')
+}
+
+const updateTask = (req,res)=>{
+    res.send('all item from the file')
+}
+
+const deleteTask = (req,res)=>{
+    res.send('all iems from the file')
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getSingleTask,
+    updateTask,
+    deleteTask
+}
